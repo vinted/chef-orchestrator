c49fb9f74c47335e5b773c7a372dae4db20f0334
Tadas <seitanas@users.noreply.github.com>
2019-02-18 09:44:02 +0200

    Specify cookbook name to search template in

--- Berksfile
+++ Berksfile
@@ -4,4 +4,5 @@ metadata
 
 group 'test' do
   cookbook 'orchestrator-test-sqlite', path: 'test/integration/cookbooks/orchestrator-test-sqlite'
+  cookbook 'orchestrator-test-mysql', path: 'test/integration/cookbooks/orchestrator-test-mysql'
 end
---
+++ test/integration/cookbooks/orchestrator-test-mysql/metadata.rb
@@ -0,0 +1,10 @@
+name 'orchestrator-test-mysql'
+maintainer 'Vinted SRE'
+maintainer_email 'sre@vinted.com'
+license 'MIT'
+description 'Installs/Configures github orchestrator'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'orchestrator'
